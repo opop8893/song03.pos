6a9b6defc7fed721099925daf08d2e2491585413
opop8893 <fffyyy810709@gmail.com>
2026-07-29 01:02:22 +0800

    加入離線功能
    Implement service worker to cache application files.

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,27 @@
+const CACHE_NAME = "songshuang-pos-v1";
 
+const filesToCache = [
+  "index.html",
+  "style.css",
+  "app.js",
+  "manifest.json"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
